926720d86401ec321635e05f2b173235b5f69785
Настя <smelw@list.ru>
2022-11-04 23:56:21 +0300

    add accordion

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 $(document).ready(function () {
-  let tabItem = $(".tab");
+  var mediaQuery = window.matchMedia("(min-width: 501px)");
 
-  tabItem.on("click", function (e) {
-    $(".tab.active").removeClass("active");
-    $(this).addClass("active");
+  if (mediaQuery.matches) {
+    let tabItem = $(".tab");
 
-    $(".tabs__content.actived").removeClass("actived");
-    $(".tabs__content[data-num=" + $(this).index() + "]").addClass("actived");
+    tabItem.on("click", function (e) {
+      $(".tab.active").removeClass("active");
+      $(this).addClass("active");
+
+      $(".tabs__content.actived").removeClass("actived");
+      $(".tabs__content[data-num=" + $(this).index() + "]").addClass("actived");
+    });
+  }
+});
+
+$(document).ready(function () {
+  $(".accordion-item__button").click(function () {
+    const parent = $(this).parent();
+
+    if (parent.hasClass("accordion-item--active")) {
+      parent.removeClass("accordion-item--active");
+    } else {
+      $(".accordion-item").removeClass("accordion-item--active");
+      parent.addClass("accordion-item--active");
+    }
   });
 });
